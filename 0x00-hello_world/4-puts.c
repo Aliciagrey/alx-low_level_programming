61b86c26c84756902330bf9fdaa26acd6e958c65
Favour Aggrey <aggreyfaith6@gmail.com>
2022-10-19 16:05:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+
+ * main -Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+
+ * Return: 0
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
+
